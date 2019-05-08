fcfaaea63bc1e6c9b2e1eb0f35dc8e75875d2530
Jordan <jordan.dillard13@gmail.com>
2019-05-07 23:48:13 -0500

    Added borders and box shadow

--- app.js
+++
@@ -1,8 +0,0 @@
-// function userFunc(){
-//   $(document).ready(function () {
-//     var userInput = document.getElementById('tweet1').value
-//     //|| document.getElementById('userSearch').value ;
-//     var filtered = streams.users[userInput]
-//     $("p1").html(JSON.stringify(filtered));
-//   })
-// }
